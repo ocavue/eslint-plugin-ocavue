4f68b09a65596153eb989f994b32c0021c90fe03
ocavue <ocavue@gmail.com>
2026-08-31 01:15:34 +1200

    refactor: add `meta.version` and `satisfies ESLint.Plugin` to the plugin entry

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
+import type { ESLint } from 'eslint'
+
+import { version } from '../package.json'
+
 import noImplicitArrowLinebreak from './rules/no-implicit-arrow-linebreak'
 
 const plugin = {
   meta: {
     name: 'eslint-plugin-ocavue',
+    version,
   },
   rules: {
     'no-implicit-arrow-linebreak': noImplicitArrowLinebreak,
   },
-}
+} satisfies ESLint.Plugin
 
 export default plugin
